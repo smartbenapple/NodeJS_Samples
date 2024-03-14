c6d33eb9ca2da883a70fa5058a7103820518314e
smartben git learning <smartbenapple@icloud.com>
2024-03-13 20:52:39 -0700

    updates to movies
    ben

---
+++ microservicesUi/src/app/movies/Movies.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+interface IMovie
+{
+  title:string,
+  year: string,
+}
+
+const url = 'https://gcloud-ms-api-axxh6chama-wl.a.run.app/movie'; // locally: http://localhost:8181/movie
+export class MovieClass
+{
+  movies = [{ "title": "Bambi", "year": "1956" }, { "title": "Snow White", "year": "1934" }];
+  logMessage:string = "...";
+
+  constructor()
+  {
+    this.movies = [{ "title": "Mary Poppins", "year": "1956" }, { "title": "Dumbo", "year": "1967" }];
+
+    this.getAll().then(items =>
+    {
+      console.log("GetAll Called.");
+      this.logMessage = "GetAll Called.";
+
+      this.movies = [{ "title": "Iron Man", "year": "2008" }, { "title": "Troy", "year": "2009" }];
+    });
+  }
+  async getAll()
+  {
+    return [{"title":"testing", "year":"2024"}];
+  }
+}
--- microservicesUi/src/app/movies/movielist.component.ts
+++ microservicesUi/src/app/movies/movielist.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { MovieClass } from './Movies';
 
 @Component({
   selector: 'app-movies',
   templateUrl: './movielist.component.html',
   styleUrl: './movielist.component.css'
 })
-export class MovieListComponent {
+export class MovieListComponent
+{
   title = 'My Movie List';
+
+  // Movies Array
+  movies = [{"title":"Bambi", "year":"1965"},{"title":"Iron Man", "year":"2008"}];
+
+  constructor()
+  {
+    const cMovie = new MovieClass();
+    this.movies = cMovie.movies;
+  }
 }
