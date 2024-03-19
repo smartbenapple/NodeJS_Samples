28b5ec14793e2a1f6f2f1a5555d1347886ac5245
smartben git learning <smartbenapple@icloud.com>
2024-03-18 22:50:11 -0700

    Updates to add Cors.
    Ben

--- gcloud_ms_users/index.ts
+++ gcloud_ms_users/index.ts
@@ -4,7 +4,14 @@ import { processAction } from './user/controller.js';
 
 const app = express();
 app.use(express.json());
-app.use(cors()); // https://blog.logrocket.com/using-axios-set-request-headers/
+
+// Define the CORS options
+// https://mufazmi.medium.com/solving-cors-issues-in-your-node-js-application-836506e63871
+const corsOptions = {
+    credentials: false,
+    origin: ['*', 'http://localhost:8080'] // Whitelist the domains you want to allow
+};
+app.use(cors(corsOptions)); // https://blog.logrocket.com/using-axios-set-request-headers/
 
 app.post('/users', processAction);
 
