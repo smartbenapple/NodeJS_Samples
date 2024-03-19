b67dfc622d804d29205001b9b3276f74a1d27058
smartben git learning <smartbenapple@icloud.com>
2024-03-18 22:50:43 -0700

    Updates for Cors.
    Ben

--- gcloud_ms_movies/index.js
+++ gcloud_ms_movies/index.js
@@ -3,6 +3,12 @@ import cors from 'cors';
 import { processAction } from './controller.js';
 const app = express();
 app.use(express.json());
+// Define the CORS options
+// https://mufazmi.medium.com/solving-cors-issues-in-your-node-js-application-836506e63871
+const corsOptions = {
+    credentials: false,
+    origin: ['*', 'http://localhost:8080'] // Whitelist the domains you want to allow
+};
 app.use(cors()); // https://blog.logrocket.com/using-axios-set-request-headers/
 //app.get('/movie', getAllAction);
 app.post('/movie', processAction);
--- gcloud_ms_movies/index.ts
+++ gcloud_ms_movies/index.ts
@@ -4,6 +4,13 @@ import { processAction } from './controller.js';
 
 const app = express();
 app.use(express.json());
+
+// Define the CORS options
+// https://mufazmi.medium.com/solving-cors-issues-in-your-node-js-application-836506e63871
+const corsOptions = {
+  credentials: false,
+  origin: ['*', 'http://localhost:8080'] // Whitelist the domains you want to allow
+};
 app.use(cors()); // https://blog.logrocket.com/using-axios-set-request-headers/
 
 //app.get('/movie', getAllAction);
